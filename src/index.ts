e9d20ae4a6503264b83b99ad30b80891a7d25d9c
Dan1el71 <ddrmhk1@gmail.com>
2024-11-06 23:21:02 -0500

    refactor: update server initialization to use PORT from config and improve logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import app from "./app";
-import { startConnection } from './database'
+import app from './app'
+import { PORT } from './config'
+import { startConnection } from './db/config'
 
 async function main() {
-
-  startConnection();
-  await app.listen(app.get('port'));
-  console.log('server on port',app.get('port'));
-
+  startConnection()
+  await app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 }
 
-   main();
+main()
